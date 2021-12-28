73a0338134294e011b639f38af2df628ef67f0b9
Alexander Odintsov <uzernaem@gmail.com>
2021-12-28 22:16:37 +0500

    Inquiries app config

--- upravdom/settings.py
+++ upravdom/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inquiries.apps.InquiriesConfig',
     'rest_framework',
     'corsheaders'
 ]
