fa0025f8b36997ec5264a58ef1a60388950c1b8c
Alexander Odintsov <uzernaem@gmail.com>
2022-01-06 12:48:33 +0500

    Admin panel access

--- inquiries/admin.py
+++ inquiries/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from .models import Profile, ToDo, Poll, Announcement, Notification, Comment, VoteOption, Vote, Property, Ownership, Image
+from .models import Profile #, ToDo, Poll, Announcement, Notification, Comment, VoteOption, Vote, Property, Ownership, Image
 
 # Register your models here.
-admin.site.register(ToDo)
-admin.site.register(Poll)
-admin.site.register(Announcement)
-admin.site.register(Notification)
 admin.site.register(Profile)
-admin.site.register(Comment)
-admin.site.register(VoteOption)
-admin.site.register(Vote)
-admin.site.register(Property)
-admin.site.register(Ownership)
-admin.site.register(Image)
+# admin.site.register(ToDo)
+# admin.site.register(Poll)
+# admin.site.register(Announcement)
+# admin.site.register(Notification)
+# admin.site.register(Comment)
+# admin.site.register(VoteOption)
+# admin.site.register(Vote)
+# admin.site.register(Property)
+# admin.site.register(Ownership)
+# admin.site.register(Image)
