412ef41936c20a0facc804ad1465ff7a82e20504
Alexander Odintsov <uzernaem@gmail.com>
2022-02-09 00:45:44 +0500

    Settings fix

--- upravdom/settings.py
+++ upravdom/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import sys
+import dj_database_url
 from pathlib import Path
 from datetime import timedelta
+from django.core.management.utils import get_random_secret_key
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#s=!$mag*6cmi8$!b9_^(1cy2u5ia^hm-izh_7h3%qv7sw^&xl'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNINDEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # Application definition
 
