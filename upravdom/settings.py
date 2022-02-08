e0222c2c020c904fc1c8bfa7f022021bd43769aa
Alexander Odintsov <uzernaem@gmail.com>
2022-02-09 00:42:01 +0500

    Settings fix

--- upravdom/settings.py
+++ upravdom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+SECRET_KEY = 'django-insecure-#s=!$mag*6cmi8$!b9_^(1cy2u5ia^hm-izh_7h3%qv7sw^&xl'
 
 # SECURITY WARNINDEBUG = os.getenv("DEBUG", "False") == "True"
 
